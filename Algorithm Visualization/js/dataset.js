b2f81dad646fa3ff9ebcae0ce9d7d1e4557d9006
chengqiangwai <1829290856@qq.com>
2019-03-28 15:42:14 +0800

    hello

--- Algorithm Visualization/js/dataset.js
+++ Algorithm Visualization/js/dataset.js
@@ -6,11 +6,6 @@ let dataset_echart = [
 for (let i = 80; i < 100; i += 2) {
     let tempI = i + Math.random();
     for (let j = 0; j < 3; j++) {
-        // dataset_echart[0].push( [
-        //     Math.ceil(tempI),
-        //     Math.ceil(100 * Math.random()),
-        //     'good'
-        // ]);
         math_stats_dataset.push({
             math_score: Math.ceil(tempI),
             stats_score: Math.ceil(100 * Math.random()),
@@ -21,11 +16,6 @@ for (let i = 80; i < 100; i += 2) {
 for (let i = 80; i < 100; i += 2) {
     let tempI = i + Math.random();
     for (let j = 0; j < 3; j++) {
-        // dataset_echart[0].push( [
-        //     Math.ceil(100 * Math.random()),
-        //     Math.ceil(tempI),
-        //     'good'
-        // ]);
         math_stats_dataset.push({
             math_score: Math.ceil(100 * Math.random()),
             stats_score: Math.ceil(tempI),
@@ -34,89 +24,6 @@ for (let i = 80; i < 100; i += 2) {
     }
 }
 
-// for (let i = 0; i < 80; i += 2) {
-//     let tempI = i + Math.random();
-//     for (let j = 0; j < 2; j++) {
-//         // dataset_echart[1].push( [
-//         //     Math.ceil(80 * Math.random()),
-//         //     Math.ceil(tempI),
-//         //     'bad'
-//         // ]);
-//         math_stats_dataset.push({
-//             math_score: Math.ceil(80 * Math.random()),
-//             stats_score: Math.ceil(tempI),
-//             performance: -1
-//         })
-//     }
-// }
-
-
-// for (let i = 0; i < 50; i+=3) {
-//     let tempI = i;
-//     let tempRandom = Math.random();
-//     dataset_echart[0].push(
-//         [
-//             tempI,
-//             100 * tempRandom + (1 - tempRandom) * (78 - tempI),
-//             'good'
-//         ]
-//     );
-// }
-
-// for (let i = 50; i < 90; i+=1) {
-//     let tempI = i;
-//     let tempRandom = Math.random();
-//     dataset_echart[0].push(
-//         [
-//             tempI,
-//             100 * tempRandom + (1 - tempRandom) * (78 - tempI),
-//             'good'
-//         ]
-//     );
-// }
-
-// for (let i = 90; i < 100; i+=3) {
-//     let tempI = i;
-//     let tempRandom = Math.random();
-//     dataset_echart[0].push(
-//         [
-//             tempI,
-//             100 * tempRandom + (1 - tempRandom) * (78 - tempI),
-//             'good'
-//         ]
-//     );
-// }
-
-// for (let i = 0; i < 30; i+=1) {
-//     let tempI = i;
-//     dataset_echart[1].push(
-//         [
-//             tempI,
-//             (78 - tempI - 15) * Math.random(),
-//             'bad'
-//         ]
-//     );
-// }
-// for (let i = 30; i < 60; i+=1) {
-//     let tempI = i;
-//     dataset_echart[1].push(
-//         [
-//             tempI,
-//             (78 - tempI - 5) * Math.random(),
-//             'bad'
-//         ]
-//     );
-// }
-// for (let i = 60; i < 78; i+=3) {
-//     let tempI = i;
-//     dataset_echart[1].push(
-//         [
-//             tempI,
-//             (78 - tempI - 5) * Math.random(),
-//             'bad'
-//         ]
-//     );
-// }
 for (let i = 40; i < 90; ) {
     let tempI = i;
     dataset_echart[0].push(
@@ -139,4 +46,16 @@ for (let i = 15; i < 55;) {
     );
     i += i / 40;
 }
-// console.log('math_stats_dataset', JSON.stringify(math_stats_dataset));
+let allWords = [
+    `假设你的大学开设了一门机器学习（ML）课程.`,
+    `课程导师发现数学或统计学好的学生表现最佳。`,
+    `随着时间的推移，积累了一些数据，包括参加课程的学生的数学成绩和统计学成绩，以及在ML课程上的表现（使用两个标签描述，“优”、“差”）。`,
+    `现在，课程导师想要判定数学、统计学分数和ML课程表现之间的关系。`,
+    `也许，基于这一发现，可以指定参加课程的前提条件。`,
+    `这一问题如何求解？让我们从表示已有数据开始。`,
+    `我们可以绘制一张二维图形，其中一根轴表示数学成绩，另一根表示统计学成绩。`,
+    `<button id="drawPlot">click to draw</button>`,
+    `这样每个学生就成了图上的一个点。`,
+    `点的颜色——<span style="color:green">绿</span>或<span style="color:red">红</span>`,
+    `表示学生在ML课程上的词表现：“优”或“差”。`
+];
