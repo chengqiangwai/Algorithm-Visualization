edb5bb413cd552ba7a5bfdfa0429adba28b199f8
chengqiangwai <1829290856@qq.com>
2019-03-29 16:29:56 +0800

    hi

--- Algorithm Visualization/js/dataset.js
+++ Algorithm Visualization/js/dataset.js
@@ -54,8 +54,19 @@ let allWords = [
     `也许，基于这一发现，可以指定参加课程的前提条件。`,
     `这一问题如何求解？让我们从表示已有数据开始。`,
     `我们可以绘制一张二维图形，其中一根轴表示数学成绩，另一根表示统计学成绩。`,
-    `<button class="normalButton" id="drawPlot" onclick="drawPlot()">click to draw</button>`,
+    `<button class="normalButton" onclick="drawPlot()">click to draw</button>`,
     `这样每个学生就成了图上的一个点。`,
     `点的颜色——<span style="color:green">绿</span>或<span style="color:red">红</span>`,
-    `表示学生在ML课程上的词表现：“优”或“差”。`
+    `表示学生在ML课程上的词表现：“优”或“差”。`,
+    `当一名学生申请加入课程时，会被要求提供数学成绩和统计学成绩。`,
+    `基于现有的数据，可以对学生在ML课程上的表现进行有根据的猜测。`,
+    `基本上我们想要的是某种“算法”，接受“评分元组”(math_score, stats_score)输入，预测学生在图中是红点还是绿点（绿/红也称为分类或标签）。`,
+    `当然，这一算法某种程度上包括了已有数据中的模式，已有数据也称为训练数据。`,
+    `在这个例子中，找到一条红聚类和绿聚类之间的<span style="color:red">直线</span>，然后判断成绩元组位于线的哪一边，是一个好算法。`,
+    `根据红绿点的分布，我们可以很容易找到很多条这样的曲线：`,
+    `<button class="normalButton" onclick="drawLine()">click to show line</button>`,
+    `这里的直线是我们的分界（separating boundary）（因为它分离了标签）或者分类器（classifier）（我们使用它分类数据点）。`,
+    `这些直线都可以将样本分类，但那一条更好呢？`,
+    `<button class="normalButton" onclick="leaveOneLine()">click to leave just one line</button>`,
+    `<button class="normalButton" onclick="drawDistanceLine()">find shortest point</button>`
 ];
--- Algorithm Visualization/js/index.js
+++ Algorithm Visualization/js/index.js
@@ -15,99 +15,102 @@ const BAD_COLOR = new echarts.graphic.RadialGradient(0.4, 0.3, 1, [{
 const GOOD_SHADOW_COLOR = 'rgba(120, 36, 50, 0.5)';
 const BAD_SHADOW_COLOR = 'rgba(25, 100, 150, 0.5)';
 
+let onlyLine = null;
+
 
 let dom = document.getElementById("container");
 let myChart = echarts.init(dom);
 option = null;
 seriesInOption = [{
-    data: [
-        [0, 100],
-        [100, 0]
-    ],
-    type: 'line'
-},
-{
-    data: [
-        [0, 95],
-        [105, 0]
-    ],
-    type: 'line'
-},
-{
-    data: [
-        [0, 105],
-        [95, 0]
-    ],
-    type: 'line'
-},
-{
-    name: 'good',
-    data: dataset_echart[0],
-    type: 'scatter',
-    symbolSize: function (data) {
-        return 14;
-        // return Math.sqrt(data[2]) / 5e2;
+        data: [
+            [0, 100],
+            [100, 0]
+        ],
+        type: 'line'
     },
-    label: {
-        emphasis: {
-            show: true,
-            formatter: function (param) {
-                return param.data[2];
-            },
-            position: 'top'
+    {
+        data: [
+            [0, 95],
+            [105, 0]
+        ],
+        type: 'line'
+    },
+    {
+        data: [
+            [0, 105],
+            [95, 0]
+        ],
+        type: 'line'
+    },
+    {
+        name: 'good',
+        data: dataset_echart[0],
+        type: 'scatter',
+        symbolSize: function (data) {
+            return 14;
+            // return Math.sqrt(data[2]) / 5e2;
+        },
+        label: {
+            emphasis: {
+                show: true,
+                formatter: function (param) {
+                    return param.data[2];
+                },
+                position: 'top'
+            }
+        },
+        itemStyle: {
+            normal: {
+                shadowBlur: 10,
+                shadowColor: 'rgba(120, 36, 50, 0.5)',
+                shadowOffsetY: 5,
+                color: new echarts.graphic.RadialGradient(0.4, 0.3, 1, [{
+                    offset: 0,
+                    color: 'rgb(129, 227, 238)'
+                }, {
+                    offset: 1,
+                    color: 'rgb(25, 183, 207)'
+                }])
+            }
         }
     },
-    itemStyle: {
-        normal: {
-            shadowBlur: 10,
-            shadowColor: 'rgba(120, 36, 50, 0.5)',
-            shadowOffsetY: 5,
-            color: new echarts.graphic.RadialGradient(0.4, 0.3, 1, [{
-                offset: 0,
-                color: 'rgb(129, 227, 238)'
-            }, {
-                offset: 1,
-                color: 'rgb(25, 183, 207)'
-            }])
-        }
-    }
-},
-{
-    name: 'bad',
-    data: dataset_echart[1],
-    type: 'scatter',
-    symbolSize: function (data) {
-        return 14;
-        // return Math.sqrt(data[2]) / 5e2;
-    },
-    label: {
-        emphasis: {
-            show: true,
-            formatter: function (param) {
-                return param.data[2];
-            },
-            position: 'top'
-        }
-    },
-    itemStyle: {
-        normal: {
-            shadowBlur: 10,
-            shadowColor: 'rgba(25, 100, 150, 0.5)',
-            shadowOffsetY: 5,
-            color: new echarts.graphic.RadialGradient(0.4, 0.3, 1, [{
-                offset: 0,
-                color: 'rgb(251, 118, 123)'
-            }, {
-                offset: 1,
-                color: 'rgb(204, 46, 72)'
-            }])
+    {
+        name: 'bad',
+        data: dataset_echart[1],
+        type: 'scatter',
+        symbolSize: function (data) {
+            return 14;
+            // return Math.sqrt(data[2]) / 5e2;
+        },
+        label: {
+            emphasis: {
+                show: true,
+                formatter: function (param) {
+                    return param.data[2];
+                },
+                position: 'top'
+            }
+        },
+        itemStyle: {
+            normal: {
+                shadowBlur: 10,
+                shadowColor: 'rgba(25, 100, 150, 0.5)',
+                shadowOffsetY: 5,
+                color: new echarts.graphic.RadialGradient(0.4, 0.3, 1, [{
+                    offset: 0,
+                    color: 'rgb(251, 118, 123)'
+                }, {
+                    offset: 1,
+                    color: 'rgb(204, 46, 72)'
+                }])
 
+            }
         }
     }
-}
 ];
 seriesInOption = [];
 option = {
+    animation: true,
     backgroundColor: new echarts.graphic.RadialGradient(0.3, 0.3, 0.8, [{
         offset: 0,
         color: '#f7f8fa'
@@ -147,6 +150,46 @@ window.addEventListener("resize", function () {
     myChart.resize();
 });
 
+let drawLine = (function () {
+    let clickedTime = 10;
+    return function () {
+        console.log('Hi, pengjingchun');
+        if (clickedTime < 1) {
+            alert('NO line to draw anymore');
+            return;
+        } else {
+            clickedTime--;
+            seriesInOption.push({
+                data: [
+                    [0, Math.ceil(90 + 20 * Math.random())],
+                    [Math.ceil(90 + 20 * Math.random()), 0]
+                ],
+                type: 'line'
+            });
+            option.series = seriesInOption;
+            myChart.setOption(option);
+        }
+    }
+})();
+
+function leaveOneLine() {
+    let numOfLine = 0;
+    for (let i = seriesInOption.length - 1; i >= 0; i--) {
+
+        if (seriesInOption[i].type === 'line') {
+            if (numOfLine === 0) {
+                onlyLine = seriesInOption[i];
+                numOfLine++;
+            } else {
+                seriesInOption.splice(i, 1);
+            }
+        }
+    }
+    console.log(seriesInOption)
+    option.series = seriesInOption;
+    myChart.setOption(option, true);
+}
+
 function drawPlot() {
     for (let i = 0; i < 2; i++) {
         seriesInOption.push({
@@ -178,6 +221,110 @@ function drawPlot() {
         option.series = seriesInOption;
         myChart.setOption(option);
     }
+}
 
+function getDistance(line, point) {
+    const X0 = line.data[0][0] !== 0 ? line.data[0][0] : line.data[1][0];
+    const Y0 = line.data[0][1] !== 0 ? line.data[0][1] : line.data[1][1];
+    console.log(line, X0, Y0);
+    const distance = Math.abs(Y0 * point[0] / X0 + point[1] - Y0) / Math.pow(Math.pow(Y0 / X0, 2) + 1, 0.5);
+    return distance;
 
+}
+
+function drawDistanceLine() {
+    let shortestDistance = Number.MAX_VALUE;
+    let indexOfShortest = -1;
+    const X0 = onlyLine.data[0][0] !== 0 ? onlyLine.data[0][0] : onlyLine.data[1][0];
+    const Y0 = onlyLine.data[0][1] !== 0 ? onlyLine.data[0][1] : onlyLine.data[1][1];
+    for (let i = 0; i < dataset_echart[0].length; i++) {
+        let tempDistance = getDistance(onlyLine, dataset_echart[0][i]);
+        let tempX = dataset_echart[0][i][0];
+        let tempY = dataset_echart[0][i][1];
+        seriesInOption.push({
+            data: [
+                [tempX, tempY],
+                [(X0 * Y0 * Y0 + X0 * X0 * tempX - X0 * Y0 * tempY) / (Y0 * Y0 + X0 * X0),
+                    (Y0 * Y0 * tempY * X0 - Y0 * X0 * X0 * tempX - tempX * X0 * X0 * Y0) / (Y0 * Y0 * X0 + X0 * X0 * X0)
+                ],
+            ],
+            type: 'line',
+            // label: {
+            //     show: true,
+            //     position:'top',
+            //     formatter: 'so handsome am i'
+            // },
+            // itemStyle : {  
+            //     normal : {  
+            //         lineStyle:{  
+            //             color:'rgba(120, 36, 50, 0.5)'
+            //         }  
+            //     }  
+            // },  
+        });
+        if (tempDistance < shortestDistance) {
+            shortestDistance = tempDistance;
+            indexOfShortest = i;
+        }
+    }
+    let X1_1 = dataset_echart[0][indexOfShortest][0];
+    let Y1_1 = dataset_echart[0][indexOfShortest][1];
+    seriesInOption.push({
+        data: [
+            [0, Y1_1 + Y0 * X1_1 / X0],
+            [X1_1 + X0 * Y1_1 / Y0, 0],
+        ],
+        type: 'line',
+        label: {
+            show: true,
+            position: 'top',
+            formatter: 'so handsome am i'
+        },
+        itemStyle: {
+            normal: {
+                lineStyle: {
+                    color: 'rgba(120, 36, 50, 0.5)'
+                }
+            }
+        },
+    });
+    option.series = seriesInOption;
+    myChart.setOption(option);
+    shortestDistance = Number.MAX_VALUE;
+    for (let i = 0; i < dataset_echart[1].length; i++) {
+        let tempDistance = getDistance(onlyLine, dataset_echart[1][i]);
+        if (tempDistance < shortestDistance) {
+            shortestDistance = tempDistance;
+            indexOfShortest = i;
+        }
+    }
+    let X1_2 = dataset_echart[1][indexOfShortest][0];
+    let Y1_2 = dataset_echart[1][indexOfShortest][1];
+    seriesInOption.push({
+        data: [
+            [0, Y1_2 + Y0 * X1_2 / X0],
+            [X1_2 + X0 * Y1_2 / Y0, 0],
+        ],
+        type: 'line',
+        label: {
+            show: true,
+            position: 'top',
+            formatter: 'so beautiful is she'
+        },
+        itemStyle: {
+            normal: {
+                lineStyle: {
+                    color: 'rgba(120, 36, 50, 0.5)'
+                }
+            }
+        },
+    }, {
+        data: [
+            [0, (Y1_1 + Y0 * X1_1 / X0 + Y1_2 + Y0 * X1_2 / X0) / 2],
+            [(X1_1 + X0 * Y1_1 / Y0 + X1_2 + X0 * Y1_2 / Y0) / 2, 0]
+        ],
+        type: 'line'
+    });
+    option.series = seriesInOption;
+    myChart.setOption(option);
 }
